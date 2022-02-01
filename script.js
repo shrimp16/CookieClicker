c19438db889925c86c3ab2bd815003431bf3ac6e
Shrimp <lcamara0916@gmail.com>
2022-02-01 13:08:55 -0100

    Changes to the way the cookies balance is displayed, upgrade cookie button is now working

--- script.js
+++ script.js
@@ -4,13 +4,45 @@ var cookiePriceView = document.getElementById("cookie-price");
 var cookiesPerSecondView = document.getElementById("cookies-per-second");
 
 var balance = 0;
-var cookiePrice = 1;
+var cookieValue = 1;
 var cookiesPerSecond = 0;
 
+//Prices
+const upgradePrice = 100;
+const sweetGrandmaPrice = 1000;
+const mixingMachinePrice = 5000;
+const cookieShopPrice = 30000;
+const cookieBuildingPrice = 300000; //300K
+const cookieIslandPrice = 1000000; //1KK
+const cookieDuperPrice = 5000000; //5KK
+const blackMagicPrice = 30000000; // 30KK
+
 var update = () => {
-    balanceView.innerHTML = `<p>Balance: ${balance} Cookies</p>`;
-    cookiePriceView.innerHTML = `<p>Cookie Price: ${cookiePrice} Cookies</p>`;
-    cookiesPerSecondView.innerHTML = `<p>Cookies/s: ${cookiesPerSecond} Cookies</p>`;
+
+    if(balance >= 1000000){
+        balanceView.innerHTML = `<p>Balance: ${balance / 1000000}KK Cookies</p>`;
+    }else if(balance >= 100000){
+        balanceView.innerHTML = `<p>Balance: ${balance / 1000}K Cookies</p>`;
+    }else{
+        balanceView.innerHTML = `<p>Balance: ${balance} Cookies</p>`;
+    }
+
+    if(cookieValue >= 1000000){
+        cookiePriceView.innerHTML = `<p>Cookie Value: ${cookieValue / 1000000}KK Cookies</p>`;
+    }else if(cookieValue >= 100000){
+        cookiePriceView.innerHTML = `<p>Cookie Value: ${cookieValue / 1000}K Cookies</p>`;
+    }else{
+        cookiePriceView.innerHTML = `<p>Cookie Value: ${cookieValue} Cookies</p>`;
+    }
+
+    if(cookiesPerSecond >= 1000000){
+        cookiesPerSecondView.innerHTML = `<p>Cookies/s: ${cookiesPerSecond / 1000000}KK Cookies</p>`;
+    }else if(cookiesPerSecond >= 100000){
+        cookiesPerSecondView.innerHTML = `<p>Cookies/s: ${cookiesPerSecond / 1000}K Cookies</p>`;
+    }else{
+        cookiesPerSecondView.innerHTML = `<p>Cookies/s: ${cookiesPerSecond} Cookies</p>`;
+    }
+
 }
 
 setInterval(() => {
@@ -19,6 +51,16 @@ setInterval(() => {
 }, 1000)
 
 $("#cookie").click( () => {
-    balance = balance + cookiePrice;
+    balance = balance + cookieValue;
     update();
+})
+
+$("#upgrade").click( () => {
+    if(balance >= upgradePrice){
+        cookieValue++;
+        balance = balance - upgradePrice;
+        update();
+    }else{
+        alert("You don't have enough cookies!");
+    }
 })
